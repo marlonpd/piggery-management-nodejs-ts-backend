c3980b8e4de5c60643f3c7657ca4de47e4f9b6b2
marlonpd <send8heremd@gmail.com>
2023-05-14 10:48:11 +0800

    Change response of request change pass

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -140,7 +140,7 @@ router.post('/request-change-password', async function(req: Request, res: Respon
         if (e) {
           res.status(500).json({ error: e.message });
         } else {
-          res.json({'msg': `An security code has been sent to your - ${email}.`});
+          res.json({'is_sent': true});
         }
       });
 
