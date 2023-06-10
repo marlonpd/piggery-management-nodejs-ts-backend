ee7da216b93d47bccd5a8bce817490b0999f1ff4
marlonpd <send8heremd@gmail.com>
2023-06-10 20:17:08 +0800

    Add fg model

---
+++ src/models/feeding_guide.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IFeedingGuide extends Document {
+  raise_id: Number;
+  from_date: Schema.Types.Date;
+  to_date: Schema.Types.Date;
+  feed_type: String;
+  feed_name: String;
+  grams: Number;
+  updated_at: Date;
+  created_at: Date;
+}
+
+const FeedingGuideSchema: Schema = new Schema({
+    raise_id: {
+      type: mongoose.Schema.Types.ObjectId, 
+      required: true
+    },
+    from_date: { type: Schema.Types.Date, required: true },
+    to_date: { type: Schema.Types.Date, required: true },
+    feed_type: { type: Schema.Types.String, required: true },
+    feed_name: { type: Schema.Types.String, required: true },
+    grams: { type: Schema.Types.Number, required: false },
+  }, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+});
+
+export default mongoose.model<IFeedingGuide>('FeedingGuide', FeedingGuideSchema);
