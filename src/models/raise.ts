1d5d1c9d88a8dad9ba65e7a876e49e5c37a78d12
marlonpd <send8heremd@gmail.com>
2023-07-08 12:36:17 +0800

    Add sow history model

--- src/models/raise.ts
+++ src/models/raise.ts
@@ -4,7 +4,14 @@ export interface IRaise extends Document {
   raise_type: TRaiseType;
   head_count: Number;
   name: string;
+  breed: string;
   hog_pen: string;
+  birth_date: Date;
+  dam_no: Number;
+  sire_no: Number;
+  ave_size_of_litter_dam: Number;
+  teats_count: Number;
+  rev_to: string;
   user: Object;
 }
 
@@ -12,7 +19,14 @@ const RaiseSchema: Schema = new Schema({
   raise_type: { type: String, required: true},
   head_count: { type: Schema.Types.Number, required: true },
   name: { type: Schema.Types.String, required: true },
+  breed: { type: Schema.Types.String, required: true },
   hog_pen: { type: Schema.Types.String, required: false },
+  birth_date: { type: Schema.Types.Date, required: true },
+  dam_no: { type: Schema.Types.Number, required: true },
+  sire_no: { type: Schema.Types.Number, required: true },
+  ave_size_of_litter_dam: { type: Schema.Types.Number, required: true },
+  teats_count: { type: Schema.Types.Number, required: true },
+  rev_to: { type: Schema.Types.String, required: true },
   user: {
     type: Schema.Types.ObjectId,
     required: true
---
+++ src/models/sow_history.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from 'mongoose';
+export interface ISowHistory extends Document {
+  raise_id:  Schema.Types.ObjectId;
+  boar_name:  Schema.Types.String;
+  boar_no: Schema.Types.Number;
+  breed_date: Schema.Types.Date;
+  estimated_farrowed_date: Schema.Types.Date;
+  actual_farrowed_date: Schema.Types.Date;
+  litter_male_count: Schema.Types.Number;
+  litter_female_count: Schema.Types.Number;
+  litter_deceased_count: Schema.Types.Number;
+  weaened_litter_male_count: Schema.Types.Number;
+  weaened_litter_female_count: Schema.Types.Number;
+  weaened_litter_deceased_count: Schema.Types.Number;
+  remarks: Schema.Types.String;
+}
+
+const SowHistorySchema: Schema = new Schema({
+  raise_id: { type:  Schema.Types.ObjectId, required: true},
+  boar_name: { type: Schema.Types.String, required: false },
+  boar_no: { type: Schema.Types.Number, required: false },
+  breed_date: { type: Schema.Types.Date, required: false },
+  estimated_farrowed_date:{ type: Schema.Types.Date, required: false },
+  actual_farrowed_date: { type: Schema.Types.Date, required: false },
+  litter_male_count: { type: Schema.Types.Number, required: false },
+  litter_female_count: { type: Schema.Types.Number, required: false },
+  litter_deceased_count: { type: Schema.Types.Number, required: false },
+  weaened_litter_male_count: { type: Schema.Types.Number, required: false },
+  weaened_litter_female_count:{ type: Schema.Types.Number, required: false },
+  weaened_litter_deceased_count: { type: Schema.Types.Number, required: false },
+  remarks: { type: Schema.Types.Date, required: false },
+},
+{
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+});
+
+export default mongoose.model<ISowHistory>('SowHistory', SowHistorySchema);
